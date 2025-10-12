0b0b5404851d5af8fbc82cb5dd9fde3c72ba90e1
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-10-12 17:25:34 -0500

    Update Carrito.jsx

--- src/pages/Carrito.jsx
+++ src/pages/Carrito.jsx
@@ -5,8 +5,6 @@ import { crearPedido } from "../api/api";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-
-// MUI
 import {
   Typography,
   Box,
