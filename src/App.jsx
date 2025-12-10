0c074a78056ee95631ef1fddbf12c477ccf87a21
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-12-10 08:00:00 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,35 +9,28 @@ import { AuthProvider } from "./context/AuthContext";
 import { CarritoProvider } from "./context/CarritoContext";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Layout from "./components/Layout";
-import { ThemeModeProvider } from "./context/ThemeContext"; // 
+import { ThemeModeProvider } from "./context/ThemeContext";
 
 function App() {
   return (
-    <ThemeModeProvider> 
+    <ThemeModeProvider>
       <BrowserRouter>
         <AuthProvider>
           <CarritoProvider>
             <Routes>
-              <Route element={<Layout />}>
+              <Route
+                element={
+                  <ProtectedRoute>
+                    <Layout />
+                  </ProtectedRoute>
+                }
+              >
                 <Route path="/" element={<Home />} />
-                <Route
-                  path="/carrito"
-                  element={
-                    <ProtectedRoute>
-                      <Carrito />
-                    </ProtectedRoute>
-                  }
-                />
-                <Route
-                  path="/pedidos"
-                  element={
-                    <ProtectedRoute>
-                      <Pedidos />
-                    </ProtectedRoute>
-                  }
-                />
+                <Route path="/carrito" element={<Carrito />} />
+                <Route path="/pedidos" element={<Pedidos />} />
                 <Route path="/producto/:id" element={<ProductoDetalle />} />
               </Route>
+
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
             </Routes>
