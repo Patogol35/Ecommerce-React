0e7f811de35efae94a4c6b2077162c1481e4e490
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-12-10 08:31:00 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import ActivateAccount from "./pages/ActivateAccount";
 import Carrito from "./pages/Carrito";
 import Pedidos from "./pages/Pedidos";
 import ProductoDetalle from "./pages/ProductoDetalle";
@@ -9,43 +10,44 @@ import { AuthProvider } from "./context/AuthContext";
 import { CarritoProvider } from "./context/CarritoContext";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Layout from "./components/Layout";
-import { ThemeModeProvider } from "./context/ThemeContext"; //
+import { ThemeModeProvider } from "./context/ThemeContext"; // 
 
 function App() {
-return (
-<ThemeModeProvider>
-<BrowserRouter>
-<AuthProvider>
-<CarritoProvider>
-<Routes>
-<Route element={<Layout />}>
-<Route path="/" element={<Home />} />
-<Route
-path="/carrito"
-element={
-<ProtectedRoute>
-<Carrito />
-</ProtectedRoute>
-}
-/>
-<Route
-path="/pedidos"
-element={
-<ProtectedRoute>
-<Pedidos />
-</ProtectedRoute>
-}
-/>
-<Route path="/producto/:id" element={<ProductoDetalle />} />
-</Route>
-<Route path="/login" element={<Login />} />
-<Route path="/register" element={<Register />} />
-</Routes>
-</CarritoProvider>
-</AuthProvider>
-</BrowserRouter>
-</ThemeModeProvider>
-);
+  return (
+    <ThemeModeProvider> 
+      <BrowserRouter>
+        <AuthProvider>
+          <CarritoProvider>
+            <Routes>
+              <Route element={<Layout />}>
+                <Route path="/" element={<Home />} />
+                <Route
+                  path="/carrito"
+                  element={
+                    <ProtectedRoute>
+                      <Carrito />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/pedidos"
+                  element={
+                    <ProtectedRoute>
+                      <Pedidos />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route path="/producto/:id" element={<ProductoDetalle />} />
+              </Route>
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+<Route path="/activate/:uid/:token" element={<ActivateAccount />} />
+            </Routes>
+          </CarritoProvider>
+        </AuthProvider>
+      </BrowserRouter>
+    </ThemeModeProvider>
+  );
 }
 
 export default App;
