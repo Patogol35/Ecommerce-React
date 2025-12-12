d527549b5317666951104f6183efc331bd8682a6
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2025-12-12 15:19:51 -0500

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -157,12 +157,12 @@ export default function Login() {
                 </InputAdornment>
               ),
               endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton onClick={togglePasswordVisibility} edge="end">
-                    {showPassword ? <VisibilityOff /> : <Visibility />}
-                  </IconButton>
-                </InputAdornment>
-              ),
+  <InputAdornment position="end">
+    <IconButton onClick={togglePasswordVisibility} edge="end">
+      {showPassword ? <Visibility /> : <VisibilityOff />}
+    </IconButton>
+  </InputAdornment>
+),
             }}
           />
 
