9288cf6eda19a0bbe295403650261aa314016837
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-02-11 17:24:18 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,51 +9,48 @@ import ProductoDetalle from "./pages/ProductoDetalle";
 
 import { AuthProvider } from "./context/AuthContext";
 import { CarritoProvider } from "./context/CarritoContext";
-import { ThemeModeProvider } from "./context/ThemeContext";
 
 import ProtectedRoute from "./components/ProtectedRoute";
 import Layout from "./components/Layout";
 
 function App() {
   return (
-    <ThemeModeProvider>
-      <BrowserRouter>
-        <AuthProvider>
-          <CarritoProvider>
-            <Routes>
-              <Route element={<Layout />}>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
+    <BrowserRouter>
+      <AuthProvider>
+        <CarritoProvider>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
 
-                <Route
-                  path="/carrito"
-                  element={
-                    <ProtectedRoute>
-                      <Carrito />
-                    </ProtectedRoute>
-                  }
-                />
+              <Route
+                path="/carrito"
+                element={
+                  <ProtectedRoute>
+                    <Carrito />
+                  </ProtectedRoute>
+                }
+              />
 
-                <Route
-                  path="/pedidos"
-                  element={
-                    <ProtectedRoute>
-                      <Pedidos />
-                    </ProtectedRoute>
-                  }
-                />
+              <Route
+                path="/pedidos"
+                element={
+                  <ProtectedRoute>
+                    <Pedidos />
+                  </ProtectedRoute>
+                }
+              />
 
-                <Route
-                  path="/producto/:id"
-                  element={<ProductoDetalle />}
-                />
-              </Route>
-            </Routes>
-          </CarritoProvider>
-        </AuthProvider>
-      </BrowserRouter>
-    </ThemeModeProvider>
+              <Route
+                path="/producto/:id"
+                element={<ProductoDetalle />}
+              />
+            </Route>
+          </Routes>
+        </CarritoProvider>
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
 
