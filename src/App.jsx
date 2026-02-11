0b5203dca09bfe3f34b47cd6f306f59736ab482e
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-02-10 19:30:43 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -11,49 +13,57 @@ import { CarritoProvider } from "./context/CarritoContext";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Layout from "./components/Layout";
 
-import { ThemeModeProvider } from "./context/ThemeContext";
+import { ThemeModeProvider, useThemeMode } from "./context/ThemeContext";
+
+/* 🔹 Wrapper para conectar ThemeContext con MUI */
+function AppTheme({ children }) {
+  const { theme } = useThemeMode();
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+}
 
 function App() {
   return (
     <ThemeModeProvider>
-      <BrowserRouter>
-        <AuthProvider>
-          <CarritoProvider>
-            <Routes>
+      <AppTheme>
+        <CssBaseline />
 
-              {/* Todas las rutas dentro del Layout muestran el menú */}
-              <Route element={<Layout />}>
-                <Route path="/" element={<Home />} />
+        <BrowserRouter>
+          <AuthProvider>
+            <CarritoProvider>
+              <Routes>
+                {/* Todas las rutas dentro del Layout muestran el menú */}
+                <Route element={<Layout />}>
+                  <Route path="/" element={<Home />} />
 
-                {/* Login y Register AHORA muestran el menú */}
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
+                  {/* Login y Register también muestran el menú */}
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/register" element={<Register />} />
 
-                <Route
-                  path="/carrito"
-                  element={
-                    <ProtectedRoute>
-                      <Carrito />
-                    </ProtectedRoute>
-                  }
-                />
+                  <Route
+                    path="/carrito"
+                    element={
+                      <ProtectedRoute>
+                        <Carrito />
+                      </ProtectedRoute>
+                    }
+                  />
 
-                <Route
-                  path="/pedidos"
-                  element={
-                    <ProtectedRoute>
-                      <Pedidos />
-                    </ProtectedRoute>
-                  }
-                />
+                  <Route
+                    path="/pedidos"
+                    element={
+                      <ProtectedRoute>
+                        <Pedidos />
+                      </ProtectedRoute>
+                    }
+                  />
 
-                <Route path="/producto/:id" element={<ProductoDetalle />} />
-              </Route>
-
-            </Routes>
-          </CarritoProvider>
-        </AuthProvider>
-      </BrowserRouter>
+                  <Route path="/producto/:id" element={<ProductoDetalle />} />
+                </Route>
+              </Routes>
+            </CarritoProvider>
+          </AuthProvider>
+        </BrowserRouter>
+      </AppTheme>
     </ThemeModeProvider>
   );
 }
