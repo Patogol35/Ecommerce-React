0c67181058bd993e2e289ae4c3ee00d145e08e8c
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-04-16 14:08:09 -0500

    Update getImagenesProducto.js

--- src/utils/getImagenesProducto.js
+++ src/utils/getImagenesProducto.js
@@ -1,21 +1,21 @@
 // 🔥 Helper centralizado para imágenes de productos
 
 const imagenesExtra = {
-  1: ["/imagenes/1-1.jpg"],
-  2: ["/imagenes/2-1.jpg"],
-  3: ["/imagenes/3-1.jpg"],
-  4: ["/imagenes/4-1.jpg"],
-  5: ["/imagenes/5-1.jpg"],
+  1: ["https://i.imgur.com/4r9ScxM.png"],
+  2: ["https://i.imgur.com/IiHG04G.png"],
+  3: ["https://i.imgur.com/hlbuAVl.png"],
+  4: ["https://i.imgur.com/Y5KJEgc.png"],
+  5: ["https://i.imgur.com/GNk8Blo.png"],
   6: ["https://i.imgur.com/BbTakwv.png"],
   7: ["https://i.imgur.com/x1cOKEH.png"],
   8: ["https://i.imgur.com/usPfhYU.png"],
   9: ["https://i.imgur.com/2moXf4X.png"],
   10: ["https://i.imgur.com/0Ra4FYX.png"],
-  11: ["/imagenes/11-1.jpg"],
-  12: ["/imagenes/12-1.jpg"],
-  13: ["/imagenes/13-1.jpg"],
-  14: ["/imagenes/14-1.jpg"],
-  15: ["/imagenes/15-1.jpg"],
+  11: ["https://i.imgur.com/9PWG1ao.png"],
+  12: ["https://i.imgur.com/Nyvob4S.png"],
+  13: ["https://i.imgur.com/10j4ANH.png"],
+  14: ["https://i.imgur.com/ChyFk6w.png"],
+  15: ["https://i.imgur.com/pXDEcTC.png"],
 };
 
 export const getImagenesProducto = (producto) => {
