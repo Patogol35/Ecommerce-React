44d5a4a9fb05b40dca0f26a0ad14c82bca755056
Jorge Patricio Santamaría Cherrez <128660919+Patogol35@users.noreply.github.com>
2026-04-17 10:31:31 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,43 +14,49 @@ import { CarritoProvider } from "./context/CarritoContext";
 import ProtectedRoute from "./components/ProtectedRoute";
 import Layout from "./components/Layout";
 
+// 🔥 IMPORTANTE
+import AuthGate from "./AuthGate";
+
 function App() {
   return (
     <GoogleOAuthProvider clientId="6793546722-48rr78ea6tsderktj36dvihijcofjm8f.apps.googleusercontent.com">
       <BrowserRouter>
         <AuthProvider>
-          <CarritoProvider>
-            <Routes>
-              <Route element={<Layout />}>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
+          {/* 🔥 BLOQUEA RENDER HASTA QUE AUTH TERMINE */}
+          <AuthGate>
+            <CarritoProvider>
+              <Routes>
+                <Route element={<Layout />}>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/register" element={<Register />} />
 
-                <Route
-                  path="/carrito"
-                  element={
-                    <ProtectedRoute>
-                      <Carrito />
-                    </ProtectedRoute>
-                  }
-                />
+                  <Route
+                    path="/carrito"
+                    element={
+                      <ProtectedRoute>
+                        <Carrito />
+                      </ProtectedRoute>
+                    }
+                  />
 
-                <Route
-                  path="/pedidos"
-                  element={
-                    <ProtectedRoute>
-                      <Pedidos />
-                    </ProtectedRoute>
-                  }
-                />
+                  <Route
+                    path="/pedidos"
+                    element={
+                      <ProtectedRoute>
+                        <Pedidos />
+                      </ProtectedRoute>
+                    }
+                  />
 
-                <Route
-                  path="/producto/:id"
-                  element={<ProductoDetalle />}
-                />
-              </Route>
-            </Routes>
-          </CarritoProvider>
+                  <Route
+                    path="/producto/:id"
+                    element={<ProductoDetalle />}
+                  />
+                </Route>
+              </Routes>
+            </CarritoProvider>
+          </AuthGate>
         </AuthProvider>
       </BrowserRouter>
     </GoogleOAuthProvider>
